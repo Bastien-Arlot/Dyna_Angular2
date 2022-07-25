4b00ee3e9267ad2c20de546f7738e11a6433f126
Bastien-Arlot <arlot.b@gmail.com>
2022-07-25 11:54:08 +0200

    Initial commit

---
+++ src/app/cocktail-container/cocktail-form/cocktail-form.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-cocktail-form',
+  templateUrl: './cocktail-form.component.html',
+  styleUrls: ['./cocktail-form.component.scss'],
+})
+export class CocktailFormComponent {
+  public cocktailForm: FormGroup = this.fb.group({
+    name: ['', Validators.required],
+    img: ['', Validators.required],
+    description: ['', Validators.required],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  public submit(): void {
+    console.log(this.cocktailForm);
+  }
+}
